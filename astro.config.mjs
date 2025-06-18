599356b519b4e9f02ca49cb450fcebab278f54aa
Shashwat Srijan <shashwat.srijan79@gmail.com>
2025-06-18 23:54:01 +0530

    Fixed Issues, Added Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,8 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from "@astrojs/sitemap";
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
     site:'https://saddled.com',
@@ -17,5 +19,12 @@ export default defineConfig({
         plugins: [tailwindcss()],
     },
     output: 'server',
+    adapter: vercel({
+        webAnalytics: {
+            enabled:false,
+        },
+        maxDuration: 8,
+        imageService: true,
+        }),
     integrations: [react(), mdx(), sitemap()]
 });
